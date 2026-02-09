6a1ed728a88c4ce45531d4b0ce6e88fd8ef6bd26
shino04 <r.shino04@pm.me>
2026-02-09 18:56:01 +0900

    chore: update pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,13 +2,23 @@ module.exports = {
   apps: [
     {
       name: "spoon-monitor",
-      script: "pnpm tsx monitor.ts",
+      script: "pnpm",
+      args: "tsx monitor.ts", // 監視用
       autorestart: true,
+      watch: false,
+      env: {
+        NODE_ENV: "production",
+      },
     },
     {
       name: "spoon-manager",
-      script: "pnpm tsx manager.ts",
+      script: "pnpm",
+      args: "tsx manager.ts", // Discord操作・更新用
       autorestart: true,
+      watch: false,
+      env: {
+        NODE_ENV: "production",
+      },
     },
   ],
 };
